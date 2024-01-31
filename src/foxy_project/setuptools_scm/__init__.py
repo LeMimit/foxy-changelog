e806dd137c8c0872850772c5c93a029840f12466
Fabien Hermitte <fab.hermitte@gmail.com>
2024-01-31 21:19:36 +0100

    refactor(project): add license header to every python files (#41)

---
+++ src/foxy_project/setuptools_scm/__init__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2010-2024 Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
+# SPDX-FileCopyrightText: 2024-present Fabien Hermitte
+#
+# SPDX-License-Identifier: MIT
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2016-2024 Michael Bryan <michaelfbryan@gmail.com> - Ken Mijime <kenaco666@gmail.com>
+# SPDX-FileCopyrightText: 2024-present Fabien Hermitte
+#
+# SPDX-License-Identifier: MIT
+
 from __future__ import annotations
 
 import logging
